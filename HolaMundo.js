93e879c3737737ffe9a1f8eb21113cd02438a5c6
Alvaro Olarte <alvaroolartes@hotmail.com>
2024-08-21 20:44:08 -0500

    5. Tipos de datos en JavaScript

--- HolaMundo.js
+++ HolaMundo.js
@@ -18,3 +18,48 @@ console.log(contador);
 
 
 const PI = 3.1416;
+
+let tiposDeDatos = [
+    {
+        primitivo:['string', 'number', 'boolean', 'null', 'undefined', 'object', 'symbol'],
+        complejos:['array', 'function', 'object']
+    }
+]
+console.log(tiposDeDatos[0].hola);
+
+//Primitivos
+//String
+let nombe = 'Tere';
+
+//number
+let edad = 25;
+
+//boolean
+let esMayorDeEdad = true;
+
+//null
+let noHayValor = null;
+
+//undefined
+let nodefinidi = undefined;
+
+// Symbol
+let simboloUnico = Symbol('miSimbolo');
+
+// bigint
+let numerGrabde = 2n;
+console.log(numerGrabde);
+
+
+//Complejos
+
+let carro = {
+    marca: 'Tesla',
+    modelo: 'Model s',    
+}
+
+//array
+let frutas = ['manzana', 'pera', 'uva'];
+
+//function
+function saludar(){}
