fc8cb0a4650bc4f8735933ec5612c98a5ed988fa
Alvaro Olarte <alvaroolartes@hotmail.com>
2024-08-21 20:50:45 -0500

    6. Creación de strings

---
+++ strings.js
@@ -0,0 +1,16 @@
+//tipo de dato: string
+
+let string1 = 'Hola Mundo';
+let string2 = "JavaScript es genial";
+let string3 = `${string1} y ${string2} feliz :)`;
+
+//console.log(string1);
+// console.log(string2);
+// console.log(string3);
+
+let frase = 'JavaScript es Extradamente genial';
+
+console.log(frase.length);
+console.log(frase.toLowerCase());
+console.log(frase.toUpperCase());
+console.log(frase.substring(0, 10));
