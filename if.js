3454389dbfa9e66bc3485c786c7bcd37f0ece039
Alvaro Olarte <alvaroolartes@hotmail.com>
2024-08-21 22:51:47 -0500

    11. Ejecución condicional: if

---
+++ if.js
@@ -0,0 +1,19 @@
+/*
+if (let  === algo){
+    código a ejecutar
+} else if{
+    código a ejecutar
+} else {
+    codigo a ejecutar
+}
+*/
+
+let nombre = 'Diego';
+
+if (nombre === 'Diego'){
+    console.log('Hola Diego');
+} else if (nombre === 'Juan'){
+    console.log('Hola Juan');
+} else {
+    console.log('No se quien eres');
+}
