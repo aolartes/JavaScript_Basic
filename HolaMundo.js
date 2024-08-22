26a1a5a09ba0369a823e67f6cc7342bfe6fc3893
Alvaro Olarte <alvaroolartes@hotmail.com>
2024-08-21 20:28:32 -0500

    4. Anatomía de una variable

---
+++ HolaMundo.js
@@ -0,0 +1,20 @@
+console.log('Hola Mundo!');
+let cajaDeAndy = 'Woody';
+console.log(cajaDeAndy);
+
+//Lo NO permitido
+let c = 'Woody';
+let cda = 'Woody';
+let pcAndy = 'Woody';
+
+//Lo Permitido
+let primerTrasteoDeAndy = 'Woody';
+let urlDelUsuario = 'https://www.google.com';
+let idDelUsuario = '1234567890';
+
+let contador = 0;
+
+console.log(contador);
+
+
+const PI = 3.1416;
